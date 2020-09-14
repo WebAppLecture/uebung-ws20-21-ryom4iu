33be5c7a055a960a9ba5a38b93348c0273559ee1
eisenmann <71225703+ryom4iu@users.noreply.github.com>
2020-09-14 14:50:06 +0200

    Finish tasks 1 to 5.
    Also renames faculty to factorial.
    
    * 01-MyMath/MyMath.js: Do it.

--- 01-MyMath/MyMath.js
+++ 01-MyMath/MyMath.js
@@ -11,7 +11,7 @@ export class MyMath {
      * 
      * @param value Unser Initialwert für den Wert von unserer MyMath Instanz.
      */
-    constructor(value) {
+    constructor(value = 0) {
         // 'this' referenziert den Kontext in dem die aktuelle Funktion aufgerufen wird. 
         // Hier referenziert es die Instanz der Klasse MyMath die wir gerade erstellen.
         // mit 'value * 1' erzwingen wir, dass value als number gelesen wird.
@@ -19,26 +19,58 @@ export class MyMath {
     }
 
     add(value) {
-
+        this.value += value * 1;
+        return this;
     }
 
     subtract(value) {
-
+        this.value -= value;
+        return this;
     }
 
     multiply(value) {
-
+        this.value *= value;
+        return this;
     }
 
     divide(value) {
-
+        if (value == 0) {
+            console.error("Division by zero");
+            return;
+        }
+        this.value /= value;
+        return this;
     }
 
     pow(value) {
-
+        if (value % 1) {
+            console.error("pow() is currently only implemented for integral exponents")
+            return;
+        } else if (value < 0) {
+            if (!this.value) {
+                console.error("pow() on a value of zero with a negative exponent")
+                return;
+            }
+            // cover negative exponents
+            this.value = 1 / this.value;
+            value = -value;
+        }
+        let x = 1;
+        while (value--)
+            x *= this.value;
+        this.value = x;
+        return this;
     }
 
-    faculty() {
-
+    factorial() {
+        if (this.value % 1 && this.value != 0 || this.value < 0) {
+            console.error("faculty() is only defined for non-negative integers");
+            return;
+        }
+        let n = 1;
+        while (this.value > 1)
+            n *= this.value--;
+        this.value = n;
+        return this;
     }
 }
