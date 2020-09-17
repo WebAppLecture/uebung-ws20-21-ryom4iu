92609b72040fcc7a04cd1e0b59af798a81a39386
eisenmann <71225703+ryom4iu@users.noreply.github.com>
2020-09-17 11:39:06 +0200

    Change button input keys.
    Use 98 (NumPad2) and 69 (E) instead of 101 (5) for the primary button,
    and 97 (NumPad1) and 81 (Q) instead of 113 (F2) for the secondary button.
    
    * 04-Games/src/js/Inputs.js: Do it.

--- 04-Games/src/js/Inputs.js
+++ 04-Games/src/js/Inputs.js
@@ -1,10 +1,10 @@
 export const Inputs = {
     "primary": {
-        "keys": [101],
+        "keys": [98, 69],
         "text": "E",
     },
     "secondary": {
-        "keys": [113],
+        "keys": [97, 81],
         "text": "Q",
     },
     "up": {
